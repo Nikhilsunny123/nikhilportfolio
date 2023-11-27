0f91beda6787014f781aa9bb83c498e50a2c6579
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-11-27 19:38:53 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./styles/main.scss"
+import { QueryClient, QueryClientProvider } from "react-query";
+import Layout from "./components/LayOut/Layout";
+import Works from "./pages/Works";
+import HomePage from "./pages/HomePage";
+import ContactMe from "./pages/ContactMe";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
+  const client = new QueryClient();
   return (
-    <div className="App">
-     
-    </div>
+    <QueryClientProvider client={client}>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="works" element={<Works />} />
+              <Route path="contactme" element={<ContactMe />} />
+              <Route path="aboutme" element={<AboutMe />} />
+
+              {/* <Route path="*" element={<ErrorPage />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </QueryClientProvider>
   );
 }
 
